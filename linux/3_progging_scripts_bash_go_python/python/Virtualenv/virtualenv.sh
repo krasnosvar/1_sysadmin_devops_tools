46131e83e302341ce1e782810f6650bc7f406caa
krasnosvar <krasnosvar@gmail.com>
2024-02-13 13:14:27 +0400

    fix before pull

--- linux/3_progging_scripts_bash_go_python/python/Virtualenv/virtualenv.sh
+++
@@ -1,7 +0,0 @@
-#Create a virtual environment by entering the directory and running:
-cd dir/
-virtualenv .
-#Enter the virtual environment
-source bin/activate
-#and install all required packages
-pip3 install -r requirements.txt
